3ee6d87172ad125cc32c3c4b139cec108ddde442
george kaltner <georgekaltner@new-host-5.home>
2015-02-20 18:40:02 -0500

    Initial Commit

---
+++ ticTacToe/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ticTacToe
+//
+//  Created by george kaltner on 2/20/15.
+//  Copyright (c) 2015 georgeKaltner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
